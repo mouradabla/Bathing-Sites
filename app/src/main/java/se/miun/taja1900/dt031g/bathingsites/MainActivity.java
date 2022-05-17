95b38d264e20c2a2ec75baba12ddd2d64f037932
mouradabla <taja1900@student.miun.se>
2022-05-17 12:34:28 +0200

    Initializing the App

---
+++ app/src/main/java/se/miun/taja1900/dt031g/bathingsites/MainActivity.java
@@ -0,0 +1,14 @@
+package se.miun.taja1900.dt031g.bathingsites;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
